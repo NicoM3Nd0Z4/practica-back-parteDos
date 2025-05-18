0dcca6c3aa47b7dceb85a52363e7959009b69031
NicoM3Nd0Z4 <a00835649@tec.mx>
2025-05-17 21:35:38 -0600

    jsjsj

--- utils/sql.js
+++ utils/sql.js
@@ -1,18 +1,18 @@
-import sql from "mssql/msnodesqlv8.js";
+// import sql from "mssql/msnodesqlv8.js";
 
-const sqlConfig = {
-  user: process.env.DB_USER, // Nombre del servidor SQL Server
-  password: process.env.DB_PWD,
-  database: process.env.DB_NAME,
-  server: process.env.DB_SERVER,
-  options: {
-    encrypt: true,
-    trustServerCertificate: true, // Para evitar errores de certificado
-  },
-};
+// const sqlConfig = {
+//   user: process.env.DB_USER, // Nombre del servidor SQL Server
+//   password: process.env.DB_PWD,
+//   database: process.env.DB_NAME,
+//   server: process.env.DB_SERVER,
+//   options: {
+//     encrypt: true,
+//     trustServerCertificate: true, // Para evitar errores de certificado
+//   },
+// };
 
-const sqlConnect = async () => {
-  return await sql.connect(sqlConfig);
-};
+// const sqlConnect = async () => {
+//   return await sql.connect(sqlConfig);
+// };
 
-export { sqlConnect, sql };
+// export { sqlConnect, sql };
