847bc0c450802ea05d3d65286551140e97d66b93
NicoM3Nd0Z4 <a00835649@tec.mx>
2025-05-26 20:17:52 -0600

    ASjdjasdjf

--- index.js
+++ index.js
@@ -24,6 +24,7 @@ console.log("Environment variables present:", {
   clientEmail: !!process.env.CLIENT_EMAIL
 });
 
+// At the end before listen
 app.use(cors());
 app.use(morgan("dev"));
 app.use(express.json());
@@ -107,7 +108,5 @@ app.get('/firebase-test', async (req, res) => {
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, '0.0.0.0', () => console.log(`Server running on port ${PORT}`));
-
-console.log("About to start server on port:", PORT);
 
 console.log('Hello, world!');
