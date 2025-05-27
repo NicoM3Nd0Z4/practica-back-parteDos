83b58dd86327840c10c724162daae29e6ff08892
NicoM3Nd0Z4 <a00835649@tec.mx>
2025-05-26 19:55:02 -0600

    jsjsjsj

--- index.js
+++ index.js
@@ -46,57 +46,57 @@ app.get('/firebase-test', async (req, res) => {
   }
 });
 
-//hashing test
-let msg = "Hello, world!";
-const hashing = crypto.createHash("sha512");
-const hash = hashing.update(msg).digest("base64url");
-console.log(hash);
+// //hashing test
+// let msg = "Hello, world!";
+// const hashing = crypto.createHash("sha512");
+// const hash = hashing.update(msg).digest("base64url");
+// console.log(hash);
 
-//Hashing Pepper
-const pepper = process.env.PEPPER;
+// //Hashing Pepper
+// const pepper = process.env.PEPPER;
 
-const newMsg = pepper + msg;
-const hashing2 = crypto.createHash("sha512");
+// const newMsg = pepper + msg;
+// const hashing2 = crypto.createHash("sha512");
 
-const hashpepper = hashing2.update(newMsg).digest("base64url");
-console.log(hashpepper);
+// const hashpepper = hashing2.update(newMsg).digest("base64url");
+// console.log(hashpepper);
 
-//Hashing Salt
-const salt = crypto.randomBytes(24);
+// //Hashing Salt
+// const salt = crypto.randomBytes(24);
 
-const newMsg2 = salt.toString(("base64url")) + msg;
-const hashing3 = crypto.createHash("sha512");
+// const newMsg2 = salt.toString(("base64url")) + msg;
+// const hashing3 = crypto.createHash("sha512");
 
-const hashSalt = hashing3.update(newMsg2).digest("base64url");
-console.log(salt + ":" + hashSalt);
+// const hashSalt = hashing3.update(newMsg2).digest("base64url");
+// console.log(salt + ":" + hashSalt);
 
-//Cifrando el hash
-const texto = "Hola mundo!";
+// //Cifrando el hash
+// const texto = "Hola mundo!";
 
-const encryption_key = "byz9VFNtbRQM0yBODcCb1lrUtVVH3D3x"; // 32 chars
-const initialization_vector = "X05IGQ5qdBnIqAWD"; // 16 chars
+// const encryption_key = "byz9VFNtbRQM0yBODcCb1lrUtVVH3D3x"; // 32 chars
+// const initialization_vector = "X05IGQ5qdBnIqAWD"; // 16 chars
 
-const cipher = crypto.createCipheriv(
-  "aes-256-cbc",
-  Buffer.from(encryption_key),
-  Buffer.from(initialization_vector)
-);
+// const cipher = crypto.createCipheriv(
+//   "aes-256-cbc",
+//   Buffer.from(encryption_key),
+//   Buffer.from(initialization_vector)
+// );
 
-let crypted = cipher.update(texto,"utf8", "hex");
-crypted += cipher.final("hex");
-console.log(crypted);
+// let crypted = cipher.update(texto,"utf8", "hex");
+// crypted += cipher.final("hex");
+// console.log(crypted);
 
-// Desencriptando el hash
-const decipher = crypto.createDecipheriv(
-  "aes-256-cbc",
-  Buffer.from(encryption_key),
-  Buffer.from(initialization_vector)
-);
-let dec = decipher.update(crypted, "hex", "utf8");
-dec += decipher.final("utf8");
-console.log(dec);
+// // Desencriptando el hash
+// const decipher = crypto.createDecipheriv(
+//   "aes-256-cbc",
+//   Buffer.from(encryption_key),
+//   Buffer.from(initialization_vector)
+// );
+// let dec = decipher.update(crypted, "hex", "utf8");
+// dec += decipher.final("utf8");
+// console.log(dec);
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, '0.0.0.0', () => console.log(`Server running on port ${PORT}`));
 
 console.log('Hello, world!');
