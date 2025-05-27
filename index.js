ecdbd76170702237150988a780801b6e684a0b58
NicoM3Nd0Z4 <a00835649@tec.mx>
2025-05-26 19:43:13 -0600

    jsjsjs

--- index.js
+++ index.js
@@ -76,6 +76,7 @@ let dec = decipher.update(crypted, "hex", "utf8");
 dec += decipher.final("utf8");
 console.log(dec);
 
-app.listen(5000, () => console.log("Server running at http://localhost:5000"));
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
 console.log('Hello, world!');
